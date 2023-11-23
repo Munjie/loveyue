7d40c8e538b0e1479816bb7f10a8033305c45817
Munjie <892790756@qq.com>
2023-11-23 19:53:39 +0800

    update time

--- 第一个/1/Love_files/functions.js
+++ 第一个/1/Love_files/functions.js
@@ -58,3 +58,105 @@ function timeElapse(date){
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
+
+function calculateDiffTime (date){
+	let startTime = Date.parse(date);
+
+//	let startTime = '1629107469000'  //2021-08-16 17:51
+
+	let end = new Date();
+
+	let endTime = Date.parse(end);
+
+	//let endTime = '1976262719000'  //2032-08-16 17:51
+
+	let runTime = parseInt((endTime - startTime) / 1000);
+
+	var year = Math.floor(runTime / 86400 / 365);
+
+	runTime = runTime % (86400 * 365);
+
+	var month = Math.floor(runTime / 86400 / 30);
+
+	runTime = runTime % (86400 * 30);
+
+	var day = Math.floor(runTime / 86400);
+
+	runTime = runTime % 86400;
+
+	var hour = Math.floor(runTime / 3600);
+
+	runTime = runTime % 3600;
+
+	var minute = Math.floor(runTime / 60);
+
+	runTime = runTime % 60;
+
+	var second = runTime;
+
+	console.log(`相差${year}年${month}月${day}天${hour}小时${minute}分${second}秒`);
+
+	//return year+','+month+','+day+','+hour+','+minute+','+second;
+
+	var result = "第 <span class=\"digit\">" + day + "</span> 天 <span class=\"digit\">" + hour + "</span> 小时 <span class=\"digit\">" + minute + "</span> 分钟 <span class=\"digit\">" + second + "</span> 秒";
+	$("#clock").html(result);
+
+}
+
+
+
+/**
+ * //获取两个日期的相差日期数
+ * @param {any} firstDate 开启时间 2022-03-23 00:03:00
+ * @param {any} secondDate 结束时间 2023-03-23 00:04:00
+ * @param {any} differ(differ 1.相差天数; 2.相差小时数; 3.相差分钟数; 4.相差秒数)
+ */
+function date_getdiffer_date(firstDate, differ) {
+	//1)将两个日期字符串转化为日期对象
+	var startDate = new Date(this.date_ios_format(firstDate));
+	var endDate = new Date();
+	//2)计算两个日期相差的毫秒数
+	var msecNum = endDate.getTime() - startDate.getTime();
+	//3)计算两个日期相差的天数
+	var dayNum = Math.floor(msecNum / this.date_getdiffer_scale(differ));
+	//console.log(firstDate + "," + secondDate + "," + differ + "," + dayNum)
+	//return dayNum;
+	var result = "第 <span class=\"digit\">" + dayNum + "</span> 天";
+	$("#clock").html(result);
+}
+
+/**
+ * @function
+ * @name date_ios_format
+ * @desc 解决日期字符串在IOS中显示为NaN的问题
+ */
+function date_ios_format(datestr) {
+	return datestr.replace(/\-/g, "/");
+}
+
+/**
+ * //获取与毫秒数的转化比例
+ * @param {any} value（1.相差天数; 2.相差小时数; 3.相差分钟数; 4.相差秒数）
+ */
+function date_getdiffer_scale(value) {
+	var format;
+	//获取转化比（天数跟毫秒数的比例）
+	if (value == 1) {
+		format = parseFloat(24 * 60 * 60 * 1000);
+	}
+	//获取转化比（小时数跟毫秒数的比例）
+	else if (value == 2) {
+		format = parseFloat(60 * 60 * 1000);
+	}
+	//获取转化比（分钟数跟毫秒数的比例）
+	else if (value == 3) {
+		format = parseFloat(60 * 1000);
+	}
+	//获取转化比（秒数跟毫秒数的比例）
+	else if (value == 4) {
+		format = parseFloat(1000);
+	}
+	return format;
+}
+
+
